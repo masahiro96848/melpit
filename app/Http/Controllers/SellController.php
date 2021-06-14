a59112dd20b9c2479b3e117fc3b21a761dafcb3a
masahiro96848 <feetwcpic9436@gmail.com>
2021-06-14 14:48:39 +0900

    商品の出品する処理を追加

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SellRequest;
+use App\Models\Item;
 use App\Models\ItemCondition;
 use App\Models\PrimaryCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class SellController extends Controller
 {
@@ -15,11 +19,29 @@ class SellController extends Controller
                 $query->orderBy('sort_no');
             }
         ])->orderBy('sort_no')->get();
-        
+
         $conditions = ItemCondition::orderBy('sort_no')->get();
 
         return view('sell')
             ->with('categories', $categories)
             ->with('conditions', $conditions);
+    }
+
+    public function sellItem(SellRequest $request)
+    {
+        $user = Auth::user();
+
+        $item                        = new Item();
+        $item->seller_id             = $user->id;
+        $item->name                  = $request->input('name');
+        $item->description           = $request->input('description');
+        $item->secondary_category_id = $request->input('category');
+        $item->item_condition_id     = $request->input('condition');
+        $item->price                 = $request->input('price');
+        $item->state                 = Item::STATE_SELLING;
+        $item->save();
+
+        return redirect()->back()
+            ->with('status', '商品を出品しました。');
     }
 }
